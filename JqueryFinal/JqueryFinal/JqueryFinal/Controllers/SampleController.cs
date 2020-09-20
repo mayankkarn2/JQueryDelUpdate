cdfc0771552c236fafefc7d16fe47c5545bf9810
mayankkarn2 <mayankkarn2@msitprogram.net>
2020-09-20 22:55:57 +0530

    Sai Kiran Code

---
+++ JqueryFinal/JqueryFinal/JqueryFinal/Controllers/SampleController.cs
@@ -0,0 +1,93 @@
+﻿using JqueryExample.Models;
+using JqueryFinal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JqueryExample.Controllers
+{
+    public class SampleController : Controller
+    {
+        // GET: Sample
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AjaxEx()
+        {
+            return View();
+        }
+        public JsonResult Fetch()
+        {
+            DBoperation D = new DBoperation();
+            List<EMPDATA> L=D.FetchEmp();
+            return Json(L, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult AjaxEx2()
+        {
+            return View();
+        }
+        public JsonResult FetchDept(int DNO)
+        {
+            DBoperation D = new DBoperation();
+          List<EMPDATA> L= D.FetchDepartment(DNO);
+            return Json(L,JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Insert()
+        {
+            return View();
+        }
+        public JsonResult InsertEmp(EMPDATA E)
+        {
+            DBoperation D = new DBoperation();
+            string S = D.Insert(E);
+            return Json(S,JsonRequestBehavior.AllowGet);
+        }
+
+
+        public ActionResult UpdateEmp()
+        {
+            return View();
+            //Json(E, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult UpdateEmpDb(EMPDATA E)
+        {
+            DBoperation D = new DBoperation();
+            string I = D.UpdateEmp(E);
+            return Json(I, JsonRequestBehavior.AllowGet);
+        }
+
+
+
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        public JsonResult deleteEmployee(int Empno)
+        {
+            DBoperation D = new DBoperation();
+            string S = D.Delete(Empno);
+            return Json(S, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public ActionResult FetchEmp()
+        {
+            return View();
+        }
+        public JsonResult Display(int Empno)
+        {
+            DBoperation D = new DBoperation();
+            EMPDATA E=D.fetchEmployee(Empno);
+            return Json(E, JsonRequestBehavior.AllowGet);
+
+        }
+
+    }
+}
---
+++ JqueryFinal/JqueryFinal/JqueryFinal/Models/DBoperation.cs
@@ -0,0 +1,115 @@
+﻿using JqueryFinal.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace JqueryExample.Models
+{
+    public class DBoperation
+    { 
+        TrainingEntities T = null;
+        public DBoperation()
+        {
+            T = new TrainingEntities(); 
+        }
+        public List<EMPDATA> FetchEmp()
+        {
+            var E = from E1 in T.EMPDATAs
+                    select E1;
+            List<EMPDATA> L = E.ToList();
+            return L;    
+                //T.EMPDATAs.ToList();
+        }
+        public List<EMPDATA> FetchDepartment(int Deptno)
+        {
+            var D = from D1 in T.EMPDATAs
+                    where D1.DEPTNO == Deptno
+                    select D1;
+            return D.ToList();
+        }
+        public string Insert(EMPDATA E)
+        {
+            try
+            {
+                T.EMPDATAs.Add(E);
+                T.SaveChanges();
+                return "1 record inserted";
+            }
+            catch (DbUpdateException D)
+            {
+                SqlException sql = (SqlException)D.GetBaseException();
+                return sql.Message;
+            }
+            }
+
+        public string Delete(int Empno)
+        {
+            try
+            {
+                var D = from D1 in T.EMPDATAs
+                        where D1.EMPNO == Empno
+                        select D1;
+
+                EMPDATA delete = D.First();
+                T.EMPDATAs.Remove(delete);
+                T.SaveChanges();
+                return "1 record deleted successfully";
+            }
+            catch
+            {
+                return "No record found";
+            }
+        }
+
+        public string UpdateEmp(EMPDATA E)
+        {
+            try
+            {
+                var E1 = from E2 in T.EMPDATAs
+                         where E2.EMPNO == E.EMPNO
+                         select E2;
+
+                EMPDATA emp = E1.First();
+                emp.ENAME = E.ENAME;
+                emp.grade = E.grade;
+                emp.JOB = E.JOB;
+                emp.SAL = E.SAL;
+                emp.COMM = E.COMM;
+                emp.DEPTNO = E.DEPTNO;
+
+                T.SaveChanges();
+                return "recored updated";
+            }
+            catch
+            {
+                return "please enter existing employee number";
+
+            }
+        }
+
+        public EMPDATA getEmp(int Empno)
+        {
+            var E = from E1 in T.EMPDATAs
+                    where E1.EMPNO == Empno
+                    select E1;
+            return E.First();
+        }
+
+
+
+
+        public EMPDATA fetchEmployee(int Empno)
+        {
+            var E = from E1 in T.EMPDATAs
+                    where E1.EMPNO == Empno
+                    select E1;
+            return E.First();
+        
+        }
+
+
+    }
+}
