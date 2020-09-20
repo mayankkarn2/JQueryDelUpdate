d72b754e2726c87c96808eedbc2eeda9c5bd7db1
mayankkarn2 <mayankkarn2@msitprogram.net>
2020-09-20 21:18:39 +0530

    Update delete function

---
+++ Mayank/jquerydemo/jquerydemo/Controllers/SampleController.cs
@@ -0,0 +1,66 @@
+﻿using jquerydemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jquerydemo.Controllers
+{
+    public class SampleController : Controller
+    {
+        // GET: Sample
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AjaxEx()
+        {
+            return View();
+        }
+        public JsonResult Fetch()
+        {
+            Dboperation D = new Dboperation();
+            List<EMPDATA> L = D.FetchEmp();
+            return Json(L, JsonRequestBehavior.AllowGet);
+            
+        }
+
+        public ActionResult AjaxEx2()
+        {
+            return View();        
+        }
+
+        public JsonResult Fetchdept(int DNO)
+        {
+            Dboperation D = new Dboperation();
+            List<EMPDATA> L = D.FetchDept(DNO);
+            return Json(L, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult AjaxEx3()
+        {
+            return View();
+        }
+
+        public JsonResult deleteEmp(int Empno)
+        {
+            Dboperation D = new Dboperation();
+            string L = D.deleteEmp(Empno);
+            return Json(L, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult update()
+        {
+            return View();
+        }
+
+        public JsonResult updateEmp(EMPDATA E)
+        {
+            Dboperation D = new Dboperation();
+            string S = D.updateEmp(E);
+            return Json(S, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ Mayank/jquerydemo/jquerydemo/Models/Dboperation.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace jquerydemo.Models
+{
+    public class Dboperation
+    {
+		trainingEntities T = null;
+		public Dboperation()
+		{
+			T = new trainingEntities();
+		}
+		public List<EMPDATA> FetchEmp()
+		{
+			var E = from E1 in T.EMPDATAs
+					select E1;
+			List<EMPDATA> L = E.ToList();
+			return L;
+		}
+
+		public List<EMPDATA> FetchDept(int deptno)
+        {
+			var D = from D1 in T.EMPDATAs
+					where D1.DEPTNO == deptno
+					select D1;
+			List<EMPDATA> L = D.ToList();
+			return L;
+        }
+
+		public string deleteEmp(int empno)
+        {
+			var D = from D1 in T.EMPDATAs
+					where D1.EMPNO == empno
+					select D1;
+            try
+            {
+				EMPDATA del = D.First();
+				T.EMPDATAs.Remove(del);
+				T.SaveChanges();
+				return "Employee Deleted";
+			}
+
+			catch(Exception E)
+            {
+				return "No data found";
+			}
+			
+		}
+
+		public string updateEmp(EMPDATA E)
+        {
+			var E1 = from E2 in T.EMPDATAs
+					 where E2.EMPNO == E.EMPNO
+					 select E2;
+			try {
+				EMPDATA emp = E1.First();
+				emp.ENAME = E.ENAME;
+				emp.COMM = E.COMM;
+				emp.DEPTNO = E.DEPTNO;
+				emp.JOB = E.JOB;
+				emp.SAL = E.SAL;
+				emp.grade = E.grade;
+				emp.MGR = E.MGR;
+				T.SaveChanges();
+				return "recored updated";
+			}
+			catch(Exception Ex)
+            {
+				return "No record found";
+            }
+			
+		}
+	}
+}
