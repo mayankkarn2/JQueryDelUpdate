03e77c852d1a57bf5a7fb5f619a8ac64d870075d
mayankkarn2 <mayankkarn2@msitprogram.net>
2020-09-21 09:37:14 +0530

    Apurav's assignment

---
+++ Apurav/Apurav_Assessment/Assessment/Assessment/Controllers/DefaultController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assessment.Models;
+
+namespace Assessment.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult UpdateEmp()
+        {
+            return View();
+        }
+        public JsonResult UpdateEmpDB(EMPDATA E)
+        {
+            DBoperations D = new DBoperations();
+            string S = D.UpdateEmp(E);
+            return Json(S, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Delete()
+        {
+            return View();
+        }
+        public JsonResult DeleteEmp(int Empno)
+        {
+            DBoperations D = new DBoperations();
+            string L = D.DeleteEmp(Empno);
+            return Json(L, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Apurav/Apurav_Assessment/Assessment/Assessment/Models/DBoperations.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assessment.Models
+{
+	public class DBoperations
+	{
+		TrainingEntities T = null;
+		public DBoperations()
+		{
+			T = new TrainingEntities();
+		}
+		public string UpdateEmp(EMPDATA E)
+		{
+			var E1 = from E2 in T.EMPDATAs
+					 where E2.EMPNO == E.EMPNO
+					 select E2;
+			try
+			{
+				EMPDATA emp = E1.First();
+				emp.JOB = E.JOB;
+				emp.MGR = E.MGR;
+				emp.SAL = E.SAL;
+				emp.COMM = E.COMM;
+				emp.DEPTNO = E.DEPTNO;
+				emp.GRADE = E.GRADE;
+				T.SaveChanges();
+				return "Updated!";
+			}
+			catch(Exception Ex)
+			{
+				return "No such record exists!";
+			}
+			
+		}
+		public string DeleteEmp(int DelEmp)
+		{
+			var D = from D1 in T.EMPDATAs
+					where D1.EMPNO == DelEmp
+					select D1;
+			try
+			{
+				EMPDATA demp = D.First();
+				T.EMPDATAs.Remove(demp);
+				T.SaveChanges();
+				return "Deleted!";
+			}
+			catch(Exception E)
+			{
+				return "No such record exists!";
+			}
+
+		}
+	}
+}
